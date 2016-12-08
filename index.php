2acd158585be085f31ef3115f8dca0e38680719c
Hassan Shabbir Ahmed Baig <hassan.ahmed@booking.com>
2016-12-08 23:46:26 +0100

    shiny new changes

--- index.php
+++ index.php
@@ -3,6 +3,9 @@
   <title>Welcome to the Planet</title>
  </head>
  <body>
- <?php echo '<p>Welcome to existence!</p>'; ?>
+<?php echo '<p style="text-align: center;"><span style="color:#008000;"><span style="font-family:trebuchet ms,helvetica,sans-serif;"><span style="font-size:36px;">Super Duper Bluper Fluper Uber Awesome App!&nbsp;</span></span></span></p>'; ?>
+
+<p>&nbsp;</p>
  </body>
 </html>
+
