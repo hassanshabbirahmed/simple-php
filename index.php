b70117160ccc0798f63c577a89d9572a395ceee8
Hassan Shabbir Ahmed Baig <hassan.ahmed@booking.com>
2016-12-09 15:18:22 +0100

    webmat-misc

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
  <body>
 <?php echo '<p style="text-align: center;"><span style="color:#008000;"><span style="font-family:trebuchet ms,helvetica,sans-serif;"><span style="font-size:36px;">Super Duper Bluper Fluper Uber Awesome App!&nbsp;</span></span></span></p>';
 
-$myfile = fopen("/etc/roster/av1shard", "r") or die("Unable to open file!");
-echo fread($myfile,filesize("/etc/roster/av1shard"));
+$myfile = fopen("/etc/roster/webmat-misc", "r") or die("Unable to open file!");
+echo fread($myfile,filesize("/etc/roster/webmat-misc"));
 fclose($myfile);
 
 ?>
