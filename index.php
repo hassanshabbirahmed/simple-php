33272129c6df8744abbd7c66b15251a7e4b578e8
Hassan Shabbir Ahmed Baig <hassan.ahmed@booking.com>
2016-12-09 15:09:40 +0100

    echoing a roster

--- index.php
+++ index.php
@@ -3,7 +3,13 @@
   <title>Welcome to the Planet</title>
  </head>
  <body>
-<?php echo '<p style="text-align: center;"><span style="color:#008000;"><span style="font-family:trebuchet ms,helvetica,sans-serif;"><span style="font-size:36px;">Super Duper Bluper Fluper Uber Awesome App!&nbsp;</span></span></span></p>'; ?>
+<?php echo '<p style="text-align: center;"><span style="color:#008000;"><span style="font-family:trebuchet ms,helvetica,sans-serif;"><span style="font-size:36px;">Super Duper Bluper Fluper Uber Awesome App!&nbsp;</span></span></span></p>';
+
+$myfile = fopen("/etc/roster/av1shard", "r") or die("Unable to open file!");
+echo fread($myfile,filesize("/etc/roster/av1shard"));
+fclose($myfile);
+
+?>
 
 <p>&nbsp;</p>
  </body>
